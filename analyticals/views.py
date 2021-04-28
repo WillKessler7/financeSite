f4ee6a46157f79b5e759b9b3dbf21d16a3c4bed8
willk <will.kessler.23@bishops.com>
2021-04-28 09:05:53 -0700

    Added: working on user model, pretty much set up login func within view

--- analyticals/views.py
+++ analyticals/views.py
@@ -4,13 +4,32 @@ from django.shortcuts import render
 from django.template import loader
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Stock, PortEntries
 
 class loginView(View):
     def get(self, request):
         # checks if data is present in the login form within the view
         if request.POST:
-            pass
+
+            # if the username imported from template's form is for logging in,
+            if 'inputedUsername' in request.POST.keys():
+                # try to authenticate the inputed username and password
+                user = authenticate(username=request.POST['inputedUsername'],\
+                password=request.POST['inputedPassword'])
+
+            # if the user was found (authenticated),
+            if user is not None:
+                # login the user
+                login(request, user)
+
+            # otherwise, if the user was not found,
+            else:
+                # failed login
+                pass
+        else:
+            logout(request)
+            
         template = loader.get_template('analyticals/loginView.html')
         context = {}
         return HttpResponse(template.render(context, request))
